627ed4bfab78987b862bf3bcaf4e018f50e6ac39
ynishi18 <34960996+ynishi18@users.noreply.github.com>
2023-05-01 13:07:42 +0000

    add. frontendの環境構築

---
+++ frontend/src/App.tsx
@@ -0,0 +1,45 @@
+import { useState, FC } from 'react';
+import logo from './assets/react.svg';
+import './App.css';
+
+const App: FC = () => {
+  const [count, setCount] = useState(0);
+
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p className="text-blue-300 bg-red-600">Hello Vite + React!</p>
+        <p>
+          <button type="button" onClick={() => setCount((c) => c + 1)}>
+            count is: {count}
+          </button>
+        </p>
+        <p>
+          Edit <code>App.tsx</code> and save to test HMR updates.
+          </p>
+        <p>
+          <a
+            className="App-link"
+            href="https://reactjs.org"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Learn React
+          </a>
+          {' | '}
+          <a
+            className="App-link"
+            href="https://vitejs.dev/guide/features.html"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Vite Docs
+          </a>
+        </p>
+      </header>
+    </div>
+);
+};
+
+export default App;
