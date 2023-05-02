773c25f71d72ecbe02dfa2dd5e0c80388c96cf92
ynishi18 <34960996+ynishi18@users.noreply.github.com>
2023-05-02 05:08:13 +0000

    update. モックアップを作成

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,45 +1,18 @@
-import { useState, FC } from 'react';
-import logo from './assets/react.svg';
-import './App.css';
+import { FC } from 'react';
+import PublicLayout from './layouts/PublicLayout';
+import Overview from './components/Overview';
+import Feature from './components/Feature';
+import Price from './components/Price';
+import Contact from './components/Contact';
 
-const App: FC = () => {
-  const [count, setCount] = useState(0);
-
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p className="text-blue-300 bg-red-600">Hello Vite + React!</p>
-        <p>
-          <button type="button" onClick={() => setCount((c) => c + 1)}>
-            count is: {count}
-          </button>
-        </p>
-        <p>
-          Edit <code>App.tsx</code> and save to test HMR updates.
-          </p>
-        <p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-          {' | '}
-          <a
-            className="App-link"
-            href="https://vitejs.dev/guide/features.html"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Vite Docs
-          </a>
-        </p>
-      </header>
-    </div>
+// ランディングページ
+const App: FC = () => (
+  <PublicLayout>
+    <Overview />
+    <Feature />
+    <Price />
+    <Contact />
+  </PublicLayout>
 );
-};
 
 export default App;
